edab800c89904ca2e79cba27ebef35b3986f6b31
tallys augusto <tallysmugetsu@gmail.com>
2019-09-19 21:52:37 -0300

    inicio da conta

---
+++ Bank/src/CriarConta.java
@@ -0,0 +1,27 @@
+
+public class CriarConta {
+
+	public static void main(String[] args) {
+		Conta conta1 = new Conta();
+		conta1.saldo = 100.00;
+		conta1.agencia = 1234;
+		conta1.numero = 123456;
+		conta1.titular = "maria";
+		
+		Conta conta2 = new Conta();
+		conta2.saldo= 100.00;
+		conta2.agencia = 1234;
+		conta2.numero = 123456;
+		conta2.titular = "maria";
+		
+		if(conta1.agencia == conta2.agencia) {
+			System.out.println("contas iguais!");
+		}else {
+			System.out.println("contas diferentes");
+		}
+		
+		System.out.println(conta1);
+		System.out.println(conta2);
+	}
+
+}
