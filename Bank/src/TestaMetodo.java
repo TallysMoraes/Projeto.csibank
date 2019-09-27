07899cda18933c3e5a57a64f2214558bb9ac7215
tallys augusto <tallysmugetsu@gmail.com>
2019-09-26 21:53:21 -0300

    testando metodo

---
+++ Bank/src/TestaMetodo.java
@@ -0,0 +1,14 @@
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+			Conta conta1 = NEW Conta();
+			conta1.deposita(100.00);
+			conta1.deposita(50);
+			
+			System.out.println("saldo: " + conta1.saldo);
+		}
+
+	}
+
+}
